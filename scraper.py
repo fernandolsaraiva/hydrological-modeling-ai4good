414c1723907ad096b38744633431265a8e600bf8
Fernando Saraiva <fernandofilhols7@gmail.com>
2024-11-02 13:48:36 -0300

    now I have a requirements.txt, because that is necessary condition for deploy with heroku

--- scraper.py
+++ scraper.py
@@ -23,10 +23,9 @@ def getNivel(date):
     year_yesterday = date_yesterday.strftime("%Y")
     month_yesterday = date_yesterday.strftime("%m")
     day_yesterday = date_yesterday.strftime("%d")
-
-    #Posto 33767 é a estação 413
-    url = f"http://sibh.daee.sp.gov.br/grafico_nivel_novo.csv?postos[]=33767&periodo={day_yesterday}/{month_yesterday}/{year_yesterday}%20-%20{day}/{month}/{year}&tipo_dados=coleta&formato=1"
-    json_value = getJson(url)
+    station = 33767 #Posto 33767 é a estação 413 (mercado municipal)
+    url_new = f"https://cth.daee.sp.gov.br/sibh/api/v1/measurements/grouped?format=csv&start_date={year_yesterday}-{month_yesterday}-{day_yesterday}%2003%3A00&end_date={year}-{month}-{day}%2002%3A59&group_type=none&transmission_type_ids%5B%5D=1&transmission_type_ids%5B%5D=2&transmission_type_ids%5B%5D=3&transmission_type_ids%5B%5D=4&transmission_type_ids%5B%5D=5&transmission_type_ids%5B%5D=6&station_prefix_ids%5B%5D={station}"
+    json_value = getJson(url_new)
     print(json_value)
     # with open(r"/Users/fernandosaraiva/Desktop/ai4good/hydrological-modeling-ai4good/data/Nivel.csv","w") as file:
     #     file.write(json_value + "\n")
