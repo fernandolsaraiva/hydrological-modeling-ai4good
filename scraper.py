033b1a4710010977c7075d67e597b0f32568cac4
Fernando Saraiva <fernandofilhols7@gmail.com>
2024-11-02 15:50:03 -0300

    wip

--- scraper.py
+++ scraper.py
@@ -3,6 +3,9 @@ import json
 import pandas as pd
 import numpy as np
 from datetime import datetime, timedelta
+import io
+import os
+import psycopg2
 
 
 def getJson(url):
@@ -27,21 +30,35 @@ def getNivel(date):
     url_new = f"https://cth.daee.sp.gov.br/sibh/api/v1/measurements/grouped?format=csv&start_date={year_yesterday}-{month_yesterday}-{day_yesterday}%2003%3A00&end_date={year}-{month}-{day}%2002%3A59&group_type=none&transmission_type_ids%5B%5D=1&transmission_type_ids%5B%5D=2&transmission_type_ids%5B%5D=3&transmission_type_ids%5B%5D=4&transmission_type_ids%5B%5D=5&transmission_type_ids%5B%5D=6&station_prefix_ids%5B%5D={station}"
     json_value = getJson(url_new)
     print(json_value)
-    # with open(r"/Users/fernandosaraiva/Desktop/ai4good/hydrological-modeling-ai4good/data/Nivel.csv","w") as file:
-    #     file.write(json_value + "\n")
-    # df = pd.read_csv(r"/Users/fernandosaraiva/Desktop/ai4good/hydrological-modeling-ai4good/data/Nivel.csv")
-    # if df.empty:
-    #     print('Não há dados disponíveis')
-    #     last_level = last_time = json_value = None
-    #     return last_level,last_time,json_value
-    # last_level = df['valor_leitura_flu'].iloc[-1]
-    # last_time = df['intervalo'].iloc[-1]
-    # print('Last available river level: ',last_level)
-    # print('Last available time: ',last_time)
-    # return last_level,last_time,json_value
+    df = pd.read_csv(io.StringIO(json_value))
+    df = df.rename(columns={'prefix': 'station', 'date': 'timestamp', 'value_flu': 'value'})
+    df.to_csv('teste.csv', index=False)
+    return df
+
+def upsertData(df):
+    data_tuples = [tuple(x) for x in df[['station', 'timestamp', 'value']].to_numpy()]
+    DATABASE_URL = os.getenv('DATABASE_URL')
+    conn = psycopg2.connect(DATABASE_URL)
+    cursor = conn.cursor()
+    upsert_query = """
+    INSERT INTO timeseries.data_station (station, timestamp, value, created_at, updated_at) 
+    VALUES (%s, %s, %s, NOW(), NOW())
+    ON CONFLICT (station, timestamp)
+    DO UPDATE SET 
+        value = EXCLUDED.value,
+        updated_at = NOW();
+    """
+    cursor.executemany(upsert_query, data_tuples)
+    conn.commit()
+    print("Dados inseridos/atualizados com sucesso na tabela timeseries.data_station!")
+    cursor.close()
+    conn.close()
+
+
 
 if __name__ == '__main__':
     date = getDate()    
-    getNivel(date)
-    #nivel,time,json_value = getNivel(date)
+    df = getNivel(date)
+    print(df)
+    upsertData(df)
     print('Concluído')
