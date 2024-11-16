876b8e3e8919aa6803cc3803d8dc859401c0f987
Fernando Saraiva <fernandofilhols7@gmail.com>
2024-11-16 13:25:35 -0300

    temporary change to download data since 2022

--- scraper.py
+++ scraper.py
@@ -73,12 +73,12 @@ def downloadDataAndUpsertMultipleStations(stations_flu, stations_plu):
     cursor.close()
     conn.close()
 
-    if last_data_date is None:
-        last_data_date = datetime(2023, 1, 1, tzinfo=pytz.UTC)
+    # if last_data_date is None:
+    #     last_data_date = datetime(2023, 1, 1, tzinfo=pytz.UTC)
 
-    last_data_date = last_data_date.astimezone(pytz.timezone('America/Sao_Paulo'))
+    # last_data_date = last_data_date.astimezone(pytz.timezone('America/Sao_Paulo'))
     today = datetime.now(pytz.timezone('America/Sao_Paulo'))
-    
+    last_data_date = datetime(2022, 1, 1, tzinfo=pytz.UTC)
     
     while last_data_date.date() < today.date():
         for station in stations_flu:
