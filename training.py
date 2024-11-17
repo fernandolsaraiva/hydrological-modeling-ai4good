b9bc22e092d7ec0858bce92854a68eac046b5490
Fernando Saraiva <fernandofilhols7@gmail.com>
2024-11-16 23:35:04 -0300

    new functions to load data

--- training.py
+++ training.py
@@ -1,33 +1,52 @@
 # %%
 import pandas as pd
-import xgboost as xgb
-from sklearn.model_selection import train_test_split
-
 from util import get_station_data_flu, get_station_data_plu, get_station_names_plu
 
+def load_pluviometric_data(start_time, end_time, station_names='all', aggregation='10-minute'):
+    df = pd.DataFrame()
+    
+    if station_names == 'all':
+        station_names = get_station_names_plu()
+    
+    for station_name in station_names:
+        station_data = get_station_data_plu(station_name, start_time, end_time, aggregation)
+        station_code = station_data['station'].iloc[0]
+        station_name = 'plu_' + str(station_code)
+        station_data = station_data.drop(columns=['station'])
+        station_data = station_data.rename(columns={'value': station_name})
+        if df.empty:
+            df = station_data
+        else:
+            df = pd.merge(df, station_data, on='timestamp', how='outer')
+    
+    return df
 
-# %%
-station_names_plu = get_station_names_plu()
+def load_fluviometric_data(start_time, end_time, station_names=['Rio Tamanduateí - Mercado Municipal'], aggregation='10-minute'):
+    df = pd.DataFrame()
+    
+    for station_name in station_names:
+        station_data = get_station_data_flu(station_name, start_time, end_time, aggregation)
+        station_code = station_data['station'].iloc[0]
+        station_name = 'flu_' + str(station_code)
+        station_data = station_data.drop(columns=['station'])
+        station_data = station_data.rename(columns={'value': station_name})
+        if df.empty:
+            df = station_data
+        else:
+            df = pd.merge(df, station_data, on='timestamp', how='outer')
+    
+    return df
 
-# %%
-# Dado um start_time e end_time, escreva um loop para baixar a série temporal de todas as estações pluviométricas e salvar em um dataframe
+def load_data(start_time, end_time, station_name_flu=['Rio Tamanduateí - Mercado Municipal']):
+    df_plu = load_pluviometric_data(start_time, end_time)
+    df_flu = load_fluviometric_data(start_time, end_time, station_name_flu)
+    
+    df_combined = pd.merge(df_plu, df_flu, on='timestamp', how='outer')
+    
+    return df_combined
+
+# Example
 start_time = '2024-06-06'
 end_time = '2024-06-16'
-df = pd.DataFrame()
-
-for station_name in station_names_plu:
-    station_data = get_station_data_plu(station_name, start_time, end_time, '10-minute')
-    station_code = station_data['station'].iloc[0]
-    station_name = 'plu_' + str(station_code)
-    station_data = station_data.drop(columns=['station'])
-    station_data = station_data.rename(columns={'value': station_name})
-    if df.empty:
-        df = station_data
-    else:
-        df = pd.merge(df, station_data, on='timestamp', how='outer')
-
-# %%
-# Display the dataframe
-print(df)
-
-# %%
+station_name_flu = ['Rio Tamanduateí - Mercado Municipal']
+df = load_data(start_time, end_time, station_name_flu)
