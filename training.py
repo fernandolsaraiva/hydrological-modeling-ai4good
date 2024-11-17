55d7c76e48c66826e0411b68c6fb6b07c006f6dd
Fernando Saraiva <fernandofilhols7@gmail.com>
2024-11-17 00:49:34 -0300

    new functions to time delay embedding

--- training.py
+++ training.py
@@ -1,7 +1,12 @@
 # %%
+import numpy as np
 import pandas as pd
-from util import get_station_data_flu, get_station_data_plu, get_station_names_plu
 
+from util import (get_station_data_flu, get_station_data_plu,
+                  get_station_names_plu)
+
+
+# %%
 def load_pluviometric_data(start_time, end_time, station_names='all', aggregation='10-minute'):
     df = pd.DataFrame()
     
@@ -45,8 +50,70 @@ def load_data(start_time, end_time, station_name_flu=['Rio Tamanduateí - Mercad
     
     return df_combined
 
+def time_delay_embedding(series: pd.Series, n_lags: int, horizon: int):
+    """
+    Time delay embedding
+    Time series for supervised learning
+    :param series: time series as pd.Series
+    :param n_lags: number of past values to used as explanatory variables
+    :param horizon: how many values to forecast
+    :return: pd.DataFrame with reconstructed time series
+    """
+    assert isinstance(series, pd.Series)
+
+    if series.name is None:
+        name = 'Series'
+    else:
+        name = series.name
+
+    n_lags_iter = list(range(n_lags, -horizon, -1))
+
+    X = [series.shift(i) for i in n_lags_iter]
+    X = pd.concat(X, axis=1).dropna()
+    X.columns = [f'{name}(t-{j - 1})'
+                 if j > 0 else f'{name}(t+{np.abs(j) + 1})'
+                 for j in n_lags_iter]
+
+    return X
+
+def time_delay_embedding_df(df: pd.DataFrame, n_lags: int, horizon: int, station_target: str = '413'):
+    """
+    Apply time delay embedding to each column in the DataFrame
+    :param df: input DataFrame with time series data
+    :param n_lags: number of past values to use as explanatory variables
+    :param horizon: how many values to forecast
+    :return: pd.DataFrame with reconstructed time series for all columns
+    """
+    embedded_df = pd.DataFrame()
+    
+    for column in df.columns:
+        if column != 'timestamp':
+            embedded_series = time_delay_embedding(df[column], n_lags, horizon)
+            embedded_df = pd.concat([embedded_df, embedded_series], axis=1)
+    
+    # Add the timestamp column back
+    embedded_df['timestamp'] = df['timestamp'].iloc[n_lags-1:len(df) - horizon].values
+
+    # Remove all future columns except for the fluviometric variable with the specified horizon
+    future_columns = [col for col in embedded_df.columns if '(t+' in col and not col.startswith(f'flu_{station_target}(t+{horizon-1})')]
+    embedded_df = embedded_df.drop(columns=future_columns)
+    
+    return embedded_df
+# %%
 # Example
-start_time = '2024-06-06'
-end_time = '2024-06-16'
+# Load data
+start_time = '2022-01-01'
+end_time = '2022-06-01'
 station_name_flu = ['Rio Tamanduateí - Mercado Municipal']
-df = load_data(start_time, end_time, station_name_flu)
+load_data_bool = False
+if load_data_bool == True:
+    df = load_data(start_time, end_time, station_name_flu)
+    df.to_csv('data/data_experimental.csv', index=False)
+# %%
+df = pd.read_csv('data/data_experimental.csv')
+# %%
+# Time Delay Embedding
+n_lags = 6
+horizon = 13
+embedded_df = time_delay_embedding_df(df, n_lags, horizon, station_target='413')
+# %%
