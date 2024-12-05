4f692a5946723bb00ff128a5bc08e476e350c990
Fernando Saraiva <fernandofilhols7@gmail.com>
2024-12-04 21:07:03 -0300

    renaming page and modifying charts

--- pages/1_Hydrological_Monitoring.py
+++ pages/1_Hydrological_Monitoring.py
@@ -1,11 +1,10 @@
-import os
-
 import pandas as pd
 import plotly.express as px
-import psycopg2
 import streamlit as st
-from util import get_station_data_flu, get_station_names
 from PIL import Image
+from datetime import timedelta
+
+from util import get_station_data_flu, get_station_names
 
 if __name__ == "__main__":
     logo = Image.open("img/logo_ifast.png")
@@ -24,9 +23,10 @@ if __name__ == "__main__":
     # Date range selector
     col1, col2 = st.columns(2)
     with col1:
-        start_date = st.date_input('Start Date')
+        start_date = st.date_input('Start Date',value=pd.to_datetime('today').date() - timedelta(days=7))
     with col2:
-        end_date = st.date_input('End Date')
+        end_date = st.date_input('End Date',value=pd.to_datetime('today').date())
+        end_date = end_date + timedelta(days=1)
 
     # Aggregation selector
     aggregation = st.selectbox('Select Aggregation', ['10-minute','Hourly' ,'Daily'])
@@ -34,5 +34,15 @@ if __name__ == "__main__":
     # Button to plot the data
     if st.button('Plot'):
         data = get_station_data_flu(station_name, start_date, end_date, aggregation)
+        data['value'] = data['value'] / 100  # Convertendo de cm para m
         fig = px.line(data, x='timestamp', y='value', title=f'River Level - {station_name}')
+        fig.update_layout(
+            yaxis_title='River level (m)',
+            title={
+                'text': f'River Level - {station_name}',
+                'x': 0.5,
+                'xanchor': 'center',
+                'font': {'size': 20} 
+            }
+        )
         st.plotly_chart(fig)
--- pages/2_Rainfall_Monitoring.py
+++ pages/2_Rainfall_Monitoring.py
@@ -1,11 +1,8 @@
-import os
-
 import pandas as pd
 import plotly.express as px
-import psycopg2
 import streamlit as st
 from PIL import Image
-
+from datetime import timedelta
 from util import get_station_data_plu, get_station_names_plu
 
 if __name__ == "__main__":
@@ -25,9 +22,10 @@ if __name__ == "__main__":
     # Date range selector
     col1, col2 = st.columns(2)
     with col1:
-        start_date = st.date_input('Start Date')
+        start_date = st.date_input('Start Date',value=pd.to_datetime('today').date() - timedelta(days=7))
     with col2:
-        end_date = st.date_input('End Date')
+        end_date = st.date_input('End Date',value=pd.to_datetime('today').date())
+        end_date = end_date + timedelta(days=1)
 
     # Aggregation selector
     aggregation = st.selectbox('Select Aggregation', ['10-minute','Hourly' ,'Daily'])
@@ -35,5 +33,14 @@ if __name__ == "__main__":
     # Button to plot the data
     if st.button('Plot'):
         data = get_station_data_plu(station_name, start_date, end_date, aggregation)
-        fig = px.line(data, x='timestamp', y='value', title=f'River Level - {station_name}')
+        fig = px.line(data, x='timestamp', y='value', title=f'Rainfall - {station_name}')
+        fig.update_layout(
+            yaxis_title='Rainfall (mm)',
+            title={
+                'text': f'Rainfall - {station_name}',
+                'x': 0.5,
+                'xanchor': 'center',
+                'font': {'size': 20} 
+            }
+        )
         st.plotly_chart(fig)
